59d1721c5b4496b4a7c8948a61945d412de86ba0
isadora <isadoracustodio690@gmail>
2023-07-28 15:53:30 -0300

    arrumei

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/cool', function(req, res, next) {
+  res.render('index', { title: 'you are so cool!' });
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.render('index', { title: 'Bem vindo ao ifc!' });
+});
+
+router.get('/cool/beach', function(req, res, next) {
+  res.render('index', { title: 'Adoro praia!' });
 });
 
 module.exports = router;
